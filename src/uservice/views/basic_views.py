029d01f0885d61fadc95e2a469a4ccbc264611ee
Andreas Skyman <andreas.skyman@molflow.com>
2016-01-12 09:28:54 +0100

    Added basic views.

---
+++ src/uservice/views/basic_views.py
@@ -0,0 +1,27 @@
+""" Basic views for REST api
+"""
+from flask import jsonify, abort, request
+from flask.views import MethodView
+
+
+class BasicView(MethodView):
+    """Base class for views"""
+    def get(self, version):
+        """GET, a commin sanity checking"""
+        if version not in ['v1', 'v2', 'v3', 'v4']:
+            abort(404)
+
+        auth = request.args.get('auth')
+        if self._authenticate(auth):
+            abort(401)
+        else:
+            return self._get_view(version)
+
+    def _get_view(self, version):
+        """Dummy method which should be over loaded by derived classes"""
+        return jsonify(Version=version)
+
+    def _authenticate(self, auth):
+        """Dummy method that should be over loaded by derived classes that
+        support/require authentication."""
+        return True
